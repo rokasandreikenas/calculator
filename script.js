75b6af88f0123f3d1f6f55e497bbb04650ce43bf
Rokas Andreikenas <rokas@10speed.com>
2022-01-11 19:57:36 +0200

    Add buttons which changes calculator color

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const resultTag = document.querySelector(".result");
 const numberButtons = document.querySelectorAll(".row button");
+const colorButtons = document.querySelector(".color-buttons");
+const calculator = document.querySelector(".calculator");
 
 let result = 0;
 
@@ -10,4 +12,29 @@ for (let i = 0; i < numberButtons.length; i++) {
     const value = button.innerText;
     resultTag.innerText += value;
   });
+}
+
+const colors = ["white", "green", "blue", "red", "yellow", "purple"];
+
+for (let i = 0; i < colors.length; i++) {
+  const color = colors[i];
+  const colorButton = document.createElement("button");
+
+  colorButton.innerText = color;
+  colorButton.style.backgroundColor = color;
+  colorButton.onclick = () => {
+    calculator.style.backgroundColor = color;
+  };
+
+  if (color === "white") {
+    colorButton.style.border = "1px solid black";
+    colorButton.onmouseover = () => {
+      colorButton.style.backgroundColor = "grey";
+    };
+    colorButton.onmouseout = () => {
+      colorButton.style.backgroundColor = color;
+    };
+  }
+
+  colorButtons.appendChild(colorButton);
 }
