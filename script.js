31808f1333927f36c64c435b9a71a5ee5449aae2
Rokas Andreikenas <rokas@10speed.com>
2022-01-13 20:02:12 +0200

    Add working action buttons

--- script.js
+++ script.js
@@ -1,38 +1,127 @@
-const resultTag = document.querySelector(".result");
+const resultTag = document.getElementById("result-input");
+const actionLineTag = document.querySelector(".action-line");
 const numberButtons = document.querySelectorAll(".row button");
 const colorButtons = document.querySelector(".color-buttons");
 const calculator = document.querySelector(".calculator");
+const additionButton = document.getElementById("addition");
+const actionButtons = document.querySelectorAll(".action-buttons button");
+// Calculator functionality
 
+let dotAdded = false;
 let result = 0;
+let actionPressed = false;
 
 for (let i = 0; i < numberButtons.length; i++) {
   const button = numberButtons[i];
 
   button.addEventListener("click", () => {
     const value = button.innerText;
-    resultTag.innerText += value;
+
+    switch (value) {
+      case "C":
+        resultTag.innerText = 0;
+        result = 0;
+        dotAdded = false;
+        break;
+      case ".":
+        if (!dotAdded) {
+          resultTag.innerText += value;
+        }
+
+        dotAdded = true;
+        break;
+
+      default:
+        if (resultTag.innerText === "0") {
+          resultTag.innerText = value;
+        } else {
+          resultTag.innerText += value;
+        }
+        break;
+    }
   });
 }
 
-const colors = ["white", "green", "blue", "red", "yellow", "purple"];
+for (let i = 0; i < actionButtons.length; i++) {
+  const actionButton = actionButtons[i];
 
-for (let i = 0; i < colors.length; i++) {
-  const color = colors[i];
+  actionButton.addEventListener("click", () =>
+    numbersAction(actionButton.value)
+  );
+}
+
+function numbersAction(symbol) {
+  const resultValue = Number(resultTag.innerText);
+
+  modifyActionLine(symbol, resultValue, result);
+  const actionValue = makeActionBySymbol(symbol, resultValue, result);
+  result = actionValue;
+
+  resultTag.innerText = 0;
+}
+
+function modifyActionLine(symbol, value, result) {
+  const actionValue = makeActionBySymbol(symbol, value, result);
+
+  actionLineTag.innerText = `${result} ${symbol} ${value} = ${actionValue}`;
+}
+
+function makeActionBySymbol(symbol, value, result) {
+  switch (symbol) {
+    case "+":
+      return result + value;
+    case "-":
+      return result - value;
+    case "*":
+      return result * value;
+    case "/":
+      return result / value;
+    default:
+      return 0;
+  }
+}
+
+// Color functionality
+
+const firstColor = {
+  code: "white",
+  label: "Balta",
+};
+
+const secondColor = {
+  code: "green",
+  label: "Zalia",
+};
+
+const thirdColor = {
+  code: "blue",
+  label: "Melyna",
+};
+
+const fourthColor = {
+  code: "red",
+  label: "Raudona",
+};
+
+const colorObjects = [firstColor, secondColor, thirdColor, fourthColor];
+
+for (let i = 0; i < colorObjects.length; i++) {
+  const color = colorObjects[i];
   const colorButton = document.createElement("button");
 
-  colorButton.innerText = color;
-  colorButton.style.backgroundColor = color;
+  colorButton.innerText = color.label;
+  colorButton.style.backgroundColor = color.code;
   colorButton.onclick = () => {
-    calculator.style.backgroundColor = color;
+    calculator.style.backgroundColor = color.code;
   };
 
-  if (color === "white") {
+  if (color.label === "Balta") {
     colorButton.style.border = "1px solid black";
     colorButton.onmouseover = () => {
       colorButton.style.backgroundColor = "grey";
     };
     colorButton.onmouseout = () => {
-      colorButton.style.backgroundColor = color;
+      colorButton.style.backgroundColor = color.code;
     };
   }
 
